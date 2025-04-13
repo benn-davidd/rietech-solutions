e941091b8fe581c4da053ea728580475e123ee3e
benn-davidd <dbenemu@outlook.com>
2025-04-13 08:47:55 +0100

    home-clients section & carousel layout

--- home.js
+++ home.js
@@ -47,4 +47,33 @@ function typeloop() {
 
 typeloop();
 
-// HOME-SERVICES
+// HOME-CLIENTS
+
+// carousel
+
+// top Slider: continuous autoplay, pauses on hover
+const topSlider = new Swiper(".top-slider", {
+  slidesPerView: 3, // Number of slides visible at once
+  spaceBetween: 1, // Space between slides in px
+  loop: true,
+  autoplay: {
+    delay: 0, // Continuous autoplay
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true, // Pauses on hover
+  },
+  speed: 3000, // Adjust speed for continuous effect
+  allowTouchMove: false, // Disable manual swiping
+});
+
+// bottom Slider: draggable and auto-advances after a certain interval
+const bottomSlider = new Swiper(".bottom-slider", {
+  slidesPerView: 3, // Number of slides visible at once
+  spaceBetween: 5, // Space between slides in px
+  loop: true,
+  autoplay: {
+    delay: 5000, // Advances every 5 seconds
+    disableOnInteraction: false, // Continues autoplay after interaction
+  },
+  speed: 800, // Transition speed
+  allowTouchMove: true, // Enable manual swiping
+});
